8a8b0f9d58a8f254f65b4329ce56e9e4adde1c45
Joni Kivimäki <joni.kivimaki@elisanet.fi>
2023-03-09 18:41:19 +0200

    Sticky injector

--- js/injectors.js
+++ js/injectors.js
@@ -4,29 +4,16 @@ $(document).ready(function () {
 });
 
 // HEADER
-$(document).ready(function() {
+$(document).ready(function () {
   $("header").load("injectors/header.html");
 });
 
 // FOOTER
-$(document).ready(function() {
+$(document).ready(function () {
   $("footer").load("injectors/footer.html");
 });
 
 // STICKY BUTTONS
-$(function() {
-  var $sticky = $(".sticky");
-  $sticky.html('<div><button class="sticky" id="sticky-phone"><i class="fa fa-phone"></i></button><button class="sticky" id="sticky-email"><i class="fa fa-envelope"></i></button></div>');
-
-  $sticky.on("click", "#sticky-phone", function() {
-    window.location.href = "tel:+358405455417";
-  });
-
-  $sticky.on("click", "#sticky-email", function() {
-    window.location.href = "mailto:samppa@liventer.fi";
-  });
+$(document).ready(function () {
+  $(".sticky").load("injectors/sticky.html");
 });
-
-
-
-

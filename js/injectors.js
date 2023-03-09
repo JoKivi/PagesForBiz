7789b31f607d4cef187afff3d7f07594bfb9eaef
Joni Kivimäki <joni.kivimaki@elisanet.fi>
2023-03-09 15:25:32 +0200

    services updated

--- js/injectors.js
+++ js/injectors.js
@@ -1,24 +1,29 @@
+//BOOTSTRAP
+$(document).ready(function () {
+  $(".bootstrap").load("injectors/bootstrap.html");
+});
+
 // HEADER
 $(document).ready(function() {
-  $('header').load('injectors/header.html');
+  $("header").load("injectors/header.html");
 });
 
 // FOOTER
 $(document).ready(function() {
-  $('footer').load('injectors/footer.html');
+  $("footer").load("injectors/footer.html");
 });
 
 // STICKY BUTTONS
 $(function() {
-  var $sticky = $('.sticky');
+  var $sticky = $(".sticky");
   $sticky.html('<div><button class="sticky" id="sticky-phone"><i class="fa fa-phone"></i></button><button class="sticky" id="sticky-email"><i class="fa fa-envelope"></i></button></div>');
 
-  $sticky.on('click', '#sticky-phone', function() {
-    window.location.href = 'tel:+358405455417';
+  $sticky.on("click", "#sticky-phone", function() {
+    window.location.href = "tel:+358405455417";
   });
 
-  $sticky.on('click', '#sticky-email', function() {
-    window.location.href = 'mailto:samppa@liventer.fi';
+  $sticky.on("click", "#sticky-email", function() {
+    window.location.href = "mailto:samppa@liventer.fi";
   });
 });
 

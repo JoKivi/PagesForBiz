5d01160810d63c2f43127130819f178c759e5dd9
Joni Kivimäki <joni.kivimaki@elisanet.fi>
2023-03-10 21:50:40 +0200

    Performance check

--- js/main.js
+++ js/main.js
@@ -1,95 +1,100 @@
+setTimeout(function() {
+  const heroText = document.querySelector('.hero-text');
+  heroText.removeAttribute('hidden');
+}, 500);
+
 // Animaatio
-function animateElementOnScroll(element, animationClass) {
+function animateElementOnScroll(element, animationClass, delay) {
   function checkIfInView() {
     const elementTop = element.getBoundingClientRect().top;
     const elementBottom = element.getBoundingClientRect().bottom;
 
     if (elementTop < window.innerHeight && elementBottom >= 0) {
-      element.classList.add(animationClass);
+        element.classList.add(animationClass);
     } else {
-      element.classList.remove(animationClass);
+        element.classList.remove(animationClass);
+      }
     }
+
+    $(window).on('scroll', checkIfInView);
   }
 
-  $(window).on('scroll', checkIfInView);
-}
-
-$('.fade-zoom-in').each(function() {
-  animateElementOnScroll(this, 'animate');
-});
-
-// GDPR
-document.getElementById('gdpr-accept').addEventListener('click', acceptCookies);
-document.getElementById('gdpr-decline').addEventListener('click', declineCookies);
-
-window.onload = function () {
-  var gdpr = getCookie('gdpr');
-  if (gdpr != 'decline' && gdpr != 'accept') {
-    showGdprPopup();
-  }
-}
-
-function showGdprPopup() {
-  var gdprPopup = document.getElementById('gdpr-container');
-  gdprPopup.style.display = 'block';
-  document.body.style.overflow = 'hidden';
-}
-
-function acceptCookies() {
-  document.cookie = "gdpr=accept; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-  var gdprPopup = document.getElementById('gdpr-container');
-  gdprPopup.style.display = 'none';
-  document.body.style.overflow = 'auto';
-}
-
-function declineCookies() {
-  document.cookie = "gdpr=decline; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-  var gdprPopup = document.getElementById('gdpr-container');
-  gdprPopup.style.display = 'none';
-  document.body.style.overflow = 'auto';
-}
-
-
-function getCookie(name) {
-  var cookieArr = document.cookie.split(";");
-  for (var i = 0; i < cookieArr.length; i++) {
-    var cookiePair = cookieArr[i].split("=");
-    if (name == cookiePair[0].trim()) {
-      return decodeURIComponent(cookiePair[1]);
-    }
-  }
-  return null;
-}
-
-// Galleria
-const galleryWindowOpener = () => {
-  window.open("gallery.html", "_self");
-};
-
-const openGalleryButton = document.getElementById("openGalleryButton");
-openGalleryButton.addEventListener("click", galleryWindowOpener);
-
-
-// SERVICES
-const thumbnailLinks = document.querySelectorAll('.d-flex');
-
-thumbnailLinks.forEach(link => {
-  link.addEventListener("click", event => {
-    event.preventDefault();
-    const pageId = link.dataset.id;
-    window.open(`${pageId}.html`, "_self");
+  $('.fade-zoom-in').each(function () {
+    animateElementOnScroll(this, 'animate');
   });
-});
 
+  // GDPR
+  document.getElementById('gdpr-accept').addEventListener('click', acceptCookies);
+  document.getElementById('gdpr-decline').addEventListener('click', declineCookies);
 
-// STICKY BACK-TO-TOP
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.documentElement.scrollTop > 1000) {
-    document.querySelector(".back-to-top").style.display = "block";
-  } else {
-    document.querySelector(".back-to-top").style.display = "none";
+  window.onload = function () {
+    var gdpr = getCookie('gdpr');
+    if (gdpr != 'decline' && gdpr != 'accept') {
+      showGdprPopup();
+    }
   }
-}
+
+  function showGdprPopup() {
+    var gdprPopup = document.getElementById('gdpr-container');
+    gdprPopup.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  }
+
+  function acceptCookies() {
+    document.cookie = "gdpr=accept; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    var gdprPopup = document.getElementById('gdpr-container');
+    gdprPopup.style.display = 'none';
+    document.body.style.overflow = 'auto';
+  }
+
+  function declineCookies() {
+    document.cookie = "gdpr=decline; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    var gdprPopup = document.getElementById('gdpr-container');
+    gdprPopup.style.display = 'none';
+    document.body.style.overflow = 'auto';
+  }
+
+
+  function getCookie(name) {
+    var cookieArr = document.cookie.split(";");
+    for (var i = 0; i < cookieArr.length; i++) {
+      var cookiePair = cookieArr[i].split("=");
+      if (name == cookiePair[0].trim()) {
+        return decodeURIComponent(cookiePair[1]);
+      }
+    }
+    return null;
+  }
+
+  // Galleria
+  const galleryWindowOpener = () => {
+    window.open("gallery.html", "_self");
+  };
+
+  const openGalleryButton = document.getElementById("openGalleryButton");
+  openGalleryButton.addEventListener("click", galleryWindowOpener);
+
+
+  // SERVICES
+  const thumbnailLinks = document.querySelectorAll('.d-flex');
+
+  thumbnailLinks.forEach(link => {
+    link.addEventListener("click", event => {
+      event.preventDefault();
+      const pageId = link.dataset.id;
+      window.open(`${pageId}.html`, "_self");
+    });
+  });
+
+
+  // STICKY BACK-TO-TOP
+  window.onscroll = function () { scrollFunction() };
+
+  function scrollFunction() {
+    if (document.documentElement.scrollTop > 1000) {
+      document.querySelector(".back-to-top").style.display = "block";
+    } else {
+      document.querySelector(".back-to-top").style.display = "none";
+    }
+  }
 
